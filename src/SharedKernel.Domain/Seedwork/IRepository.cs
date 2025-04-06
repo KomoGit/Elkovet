4c4195c314f6e96d7c518e45d08d79e1ad2ad2e1
Elnur <63106963+KomoGit@users.noreply.github.com>
2025-04-06 12:15:53 +0400

    Generic Repository Pattern

---
+++ src/SharedKernel.Domain/Seedwork/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using System.Linq.Expressions;
+
+namespace SharedKernel.Domain.Seedwork
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        IUnitOfWork UnitOfWork { get; }
+        Task<T> AddAsync(T entity);
+        Task<T> GetAsync(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includes);
+        Task<T> GetWhere(Expression<Func<T, bool>> predicate, Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null);
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includes);
+        T Update(T entity);
+        bool Delete(T entity);
+    }
+}
